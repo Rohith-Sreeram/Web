2a5d33668f13aa3896d944672802c6203a764b38
Rohith-Sreeram <rohithking07246@gmail.com>
2025-03-01 21:17:43 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import time
+st.write("### HELLO PANDU")
+with st.spinner("Wait cheyyi pandamma"):
+    time.sleep(5)
+    
+st.image("chinni_pandamma.jpg")
+for x in range(1,6):
+    st.write("## Love you bujji kanna")
+st.balloons()
